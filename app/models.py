b03c044f57ab0015045c2470ca72a8b00aa20dd5
KelvinVillago <kelvinvillago@gmail.com>
2023-08-16 18:15:29 -0700

    Initial work for day2

--- app/models.py
+++ app/models.py
@@ -1,16 +1,41 @@
-from app import db
+from app import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from random import randint
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(75), nullable=False, unique=True)
+    username = db.Column(db.String(75), nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    posts = db.relationship('Phone', backref='author')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.password = generate_password_hash(kwargs.get('password'))
+
+    def __repr__(self):
+        return f"<User {self.id}|{self.username}>"
+    
+    def check_password(self, password_guess):
+        return check_password_hash(self.password, password_guess)
+
+@login.user_loader
+def load_user(user_id):
+    return db.session.get(User, user_id)
+
+class Phone(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     address = db.Column(db.String(75), nullable=False, unique=True)
     phoneNum = db.Column(db.String(75), nullable=False, unique=True)
     dateCreated = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)  # SQL - FOREIGN KEY(user_id) REFERENCES user(id)
 
     def __repr__(self):
         return f"<User {self.id}|{self.first_name} {self.last_name}>"
-
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,78 @@
 from app import app, db
-from flask import render_template, redirect, url_for
-from app.forms import AddContact
-from app.models import User
-
+from flask import render_template, redirect, url_for, flash
+from app.forms import SignUpForm, AddContact, LoginForm
+from app.models import User, Phone
+from flask_login import login_user, logout_user, login_required, current_user
 
 #add a route
 @app.route('/')
 def index():
-    return render_template('index.html')
+    nums = db.session.execute(db.select(Phone)).scalars().all()
+    return render_template('index.html', nums=nums)
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        print(first_name, last_name, username, email, password)
+        
+        #check user table to see if any users with username or email
+        checkUser = db.session.execute(db.select(User).where( (User.username==username) | (User.email==email))).scalar()
+        if checkUser:
+            flash('A user with that password already exists')
+            return redirect(url_for('signup'))
+
+        #create new user
+        newUser = User(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+        
+        #add user to database
+        db.session.add(newUser)
+        db.session.commit()
+
+        flash(f'{newUser.username} has been created')
+
+        #login user
+        login_user(newUser)
+
+        #return to home page
+        return redirect(url_for('index'))
+
+    return render_template('signup.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    flash('You have successfully logged out')
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        # Query the User table for a user with that username
+        user = db.session.execute(db.select(User).where(User.username==username)).scalar()
+        # If we have a user AND the password is correct for that user
+        if user is not None and user.check_password(password):
+            # log the user in via login_user function
+            login_user(user)
+            flash('You have successfully logged in')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username and\or password')
+            return redirect(url_for('login'))
+    return render_template('login.html', form=form)
 
 
 @app.route('/addcontact', methods=["GET", "POST"])
+@login_required
 def addContact():
     form = AddContact()
     if form.validate_on_submit():
@@ -20,7 +82,7 @@ def addContact():
         address = form.address.data
         
         #create new user
-        newUser = User(first_name=first_name, last_name=last_name, phoneNum=phoneNum, address=address)
+        newUser = Phone(first_name=first_name, last_name=last_name, phoneNum=phoneNum, address=address, user_id=current_user.id)
         
         #add user to database
         db.session.add(newUser)
