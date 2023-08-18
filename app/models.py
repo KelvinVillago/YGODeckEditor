d3e11680bbd497496993748bb6c81588abda98d5
KelvinVillago <kelvinvillago@gmail.com>
2023-08-17 21:17:23 -0700

    Added start of api portion

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
+import os
+import base64
 from app import db, login
-from datetime import datetime
+from datetime import datetime, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 from random import randint
 from flask_login import UserMixin
@@ -13,6 +15,8 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     posts = db.relationship('Phone', backref='author')
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration=db.Column(db.DateTime)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -24,6 +28,29 @@ class User(db.Model, UserMixin):
     def check_password(self, password_guess):
         return check_password_hash(self.password, password_guess)
 
+    def to_dict(self):
+        return {
+            'id':self.id,
+            'first_name':self.first_name,
+            'last_name':self.last_name,
+            'email':self.email,
+            'username':self.username
+        }
+    
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        else:
+            self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+            self.token_expiration = now + timedelta(seconds=expires_in)
+            db.session.commit()
+            return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow - timedelta(seconds=1)
+        db.session.commit()
+
 @login.user_loader
 def load_user(user_id):
     return db.session.get(User, user_id)
@@ -32,10 +59,20 @@ class Phone(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    address = db.Column(db.String(75), nullable=False, unique=True)
-    phoneNum = db.Column(db.String(75), nullable=False, unique=True)
+    address = db.Column(db.String(75), nullable=False)
+    phoneNum = db.Column(db.String(75), nullable=False)
     dateCreated = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)  # SQL - FOREIGN KEY(user_id) REFERENCES user(id)
 
     def __repr__(self):
         return f"<User {self.id}|{self.first_name} {self.last_name}>"
+
+    def to_dict(self):
+        return {
+            'first_name':self.first_name,
+            'last_name':self.last_name,
+            'phone_number':self.phoneNum,
+            'address': self.address,
+            'user_id':self.user_id,
+            'date_created':self.dateCreated
+        }
